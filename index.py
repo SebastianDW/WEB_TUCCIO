94041a084a88c2ba58d209583488264b0aaf9a0f
edube19 <92874291+edube19@users.noreply.github.com>
2024-05-14 15:07:04 -0500

    Add files via upload

--- index.py
+++ index.py
@@ -5,7 +5,8 @@ app = Flask(__name__)#verfiica que está en el archivo main
 
 @app.route('/')
 def principal():
-    return render_template('index.html')
+    #return render_template('index.html')
+    render_template('base.html')
 
 @app.route('/form_popup')
 def principal_form_popup():
